af4134c262025c744ec18eefd7d9091dfcda69aa
Egor Petruschenko <jaguar.94@mail.ru>
2024-10-05 12:04:36 +0600

    add get_youtube_data

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 
 from config import config
 from utils import get_youtube_data, create_database, save_data_to_database
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     api_key = os.getenv('YT_API_KEY')
     channel_ids = [
         'UC-OVMPlMA3-YCIeg4z5z23A',  # moscowpython
@@ -13,8 +15,9 @@ def main():
     params = config()
 
     data = get_youtube_data(api_key, channel_ids)
-    create_database('youtube', params)
-    save_data_to_database(data, 'youtube', params)
+    print(data)
+    # create_database('youtube', params)
+    # save_data_to_database(data, 'youtube', params)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,8 +1,34 @@
 from typing import Any
+from googleapiclient.discovery import build
 
 
 def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any]]:
     """ Получение данных о каналах и видео с помощью API YouTube. """
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    data = []
+    videos_data = []
+    next_page_token = None
+    for channel_id in channel_ids:
+        # Получение информации о каналах
+        channel_data = youtube.channels().list(part='snippet,statistics', id=channel_id).execute()
+
+        while True:
+            # Получение видео
+            response = youtube.search().list(part='id,snippet', channelId=channel_id, type='video',
+                                             order='date', maxResults=50, pageToken=next_page_token).execute()
+            videos_data.extend(response['items'])
+
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+        data.append({
+            'channel': channel_data['items'][0],
+            'videos': videos_data
+        })
+
+    return data
 
 
 def create_database(database: str, params: dict) -> None:
